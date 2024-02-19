94ffb2f161465bd4ac95a9ee30f9f4d98c780f84
Agon Haxhani <it.arprishtine@gmail.com>
2024-02-19 21:06:19 +0100

    Added trello on website

--- src/components/Trello/Trello.tsx
+++ src/components/Trello/Trello.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import './style.scss';
+import {Book} from "../../shared/interfaces";
 
 function Trello() {
+    const [showMainTrello, setShowMainTrello] = useState<boolean>(true);
+
     return (
         <div className='container'>
-            <iframe src={'https://cheery-squirrel-89cc01.netlify.app/account/trello'}/>
+            <button onClick={() => setShowMainTrello(!showMainTrello)}>{showMainTrello ? 'Kliko per Trellon e Ndihmesit' : 'Kliko per Trellon e Keshillit'}</button>
+            {showMainTrello ?
+                <iframe src={'https://cheery-squirrel-89cc01.netlify.app/account/trello'}/>
+                :
+                <iframe src={'https://cheery-squirrel-89cc01.netlify.app/account/trello/ndihmesit'}/>
+            }
         </div>
     );
 }
